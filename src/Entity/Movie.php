f393125414140edddb1ecc72dd0f37b3ff9ecc90
FranValderrey <fran.valderrey@gmail.com>
2023-11-12 13:10:03 +0100

    Tercer commit: Creación de bbdd

---
+++ src/Entity/Movie.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private array $full_details = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFullDetails(): array
+    {
+        return $this->full_details;
+    }
+
+    public function setFullDetails(array $full_details): static
+    {
+        $this->full_details = $full_details;
+
+        return $this;
+    }
+}
